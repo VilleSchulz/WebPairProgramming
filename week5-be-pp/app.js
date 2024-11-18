36fac8f527b8ca2154cdeb0a2b1d95592175638f
Ville <villesc@gmail.com>
2024-11-18 15:26:46 +0200

    pairprog ready

--- week5-be-pp/app.js
+++ week5-be-pp/app.js
@@ -1,9 +1,14 @@
+require("dotenv").config();
+const port = process.env.PORT || 4000;
 const express = require("express");
-const connectDB = require("./config/db"); 
+const connectDB = require("./config/db");
 const app = express();
 const tourRouter = require("./routes/tourRouter");
 const userRouter = require("./routes/userRouter");
-const { unknownEndpoint } = require("./middleware/customMiddleware");
+const {
+  unknownEndpoint,
+  errorHandler,
+} = require("./middleware/customMiddleware");
 
 const morgan = require("morgan");
 app.use(morgan("dev"));
@@ -16,13 +21,17 @@ app.use("/api/tours", tourRouter);
 
 // Use the userRouter for all /users routes
 app.use("/api/users", userRouter);
+// Example route that throws an error
+app.get("/error", (req, res, next) => {
+  // Trigger an error
+  const error = new Error("Something went wrong!");
+  next(error);
+});
 
 app.use(unknownEndpoint);
 // app.use(errorHandler);
-
-const port = process.env.PORT || 4000;
+app.use(errorHandler);
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
- 
