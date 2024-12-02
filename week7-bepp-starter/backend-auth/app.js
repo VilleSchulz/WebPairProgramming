f1f5592cd718976b9aace9fa5e60c0b6ceb96c58
Ville <villesc@gmail.com>
2024-12-02 20:21:40 +0200

    all test done

--- week7-bepp-starter/backend-auth/app.js
+++ week7-bepp-starter/backend-auth/app.js
@@ -24,6 +24,4 @@ app.use(errorHandler);
 
 module.exports = app;
 
-// app.listen(process.env.PORT, () => {
-//   console.log(`Server running on port ${process.env.PORT}`)
-// })  
+
--- week7-bepp-starter/backend-auth/index.js
+++ week7-bepp-starter/backend-auth/index.js
@@ -5,7 +5,10 @@ const logger = require("./utils/logger");
 
 const server = http.createServer(app);
 
-// const PORT = config.PORT || 4000; 
+// const PORT = config.PORT || 4000;
 server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
+});
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });
